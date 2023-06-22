b0d951b5ae656e1557439d8fe33fc29dc087ac4e
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-06-22 06:14:22 +0300

    first commit

---
+++ api.ts
@@ -0,0 +1,52 @@
+
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const  AWS = require('aws-sdk');
+const mysql = require('mysql');
+const dotenv = require('dotenv').config();
+
+
+const connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+
+const s3 = new AWS.S3({
+  region: 'eu-north-1',
+  apiVersion: '2006-03-01',
+  credentials: {
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+  },
+});
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  return res.status(200).send("It's working");
+});
+
+app.post('/file', async function(req,res) {
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+    console.log('Connected to database.');
+  });
+  const use = "USE kaleva;";
+  const sql= "INSERT INTO COMPANY VALUES (5, 'seura1')";
+  connection.query(use);
+  connection.query(sql);
+  return res.status(200).send("It's working");
+});
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import kuva from './kuva.png';
+import './styles.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ backgroundImage: `url(${kuva})`, backgroundRepeat: 'no-repeat', minHeight: '100%', height: '100vh', backgroundSize:'cover' }}>
+        <div style={{ display:'flex', flexDirection: 'row', justifyContent: 'flex-end', width:'100%', height: '10%', marginLeft: -260}}>
+            <h5 style={{margin: 5}}>Kirjaudu</h5>
+            <h5 style={{margin: 5}}>Rekistöröidy</h5>
+            <h5 style={{margin: 5}}>info</h5>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', width: '92%', height: '90%'}}>
+          <div style={{marginLeft: '60%'}}>
+            <h1 style={{color:'white'}}>Kaleva2024</h1>
+            <h2 style={{marginTop: -30}}>Ilmotautuminen</h2>
+            <hr></hr>
+            <h3 style={{color:'white'}}>
+              So far we have looked at the behavior when our flex-direction is row, and while working in a language
+              written top to bottom. This means that the main axis runs along the row horizontally, and our cross
+              axis alignment moves the items up and down.
+            </h3>
+          </div>
+          
+         
+          
+        </div>
+      </div>
   );
 }
 
