1d11c3bac771cafcc78d754a93616fa16257c97e
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-08-14 12:18:55 +0300

    jotain korjauksia

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -15,20 +15,24 @@ const Main = () => {
             >
               <h5 style={{margin: 5}}> Admin</h5>
             </NavLink>
-            <NavLink
-                to="/registration"
-            >
-              <h5 style={{margin: 5}}>Ilmoittaudu</h5>
-            </NavLink>
+            
         </div>
         <div style={{ display: 'flex', alignItems: 'center', width: '92%', height: '90%'}}>
           <div style={{marginLeft: '60%'}}>
             <h1 style={{color:'white'}}>Kalevan kisat 2024</h1>
-            <h2 style={{marginTop: -30}}>Ilmotautuminen</h2>
+            <h2 style={{marginTop: -30}}> Ilmoittautuminen | Anmälan</h2>
             <hr></hr>
             <h3 style={{color:'white'}}>
             "Ilmoittaudu tässä Vaasan 2024 Kalevan kisojen vapaaehtoiseksi toimitsijaksi. Saat kuittauksen ilmoittautumisesta annettuun sähköpostiin. Otamme sinuun yhteyttä. Tervetuloa mukaan! | Här anmäler Du dig som frivillig funktionär till Kalevan kisat 2024 i Vasa. Du får en kvittering på gjord anmälan till den epost som angivits. Vi kontaktar Dig. Välkommen med!
             </h3>
+            <button>
+              <NavLink
+                  to="/registration"
+              >
+                <h5 style={{margin: 5}}>Ilmoittaudu</h5>
+              </NavLink>
+            </button>
+           
           </div>
         </div>
       </div>
