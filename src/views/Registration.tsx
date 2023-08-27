b69a7db09d5f7287d5ac39794d53522bb91b94b4
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-08-27 10:28:47 +0300

    korjaus

--- src/views/Registration.tsx
+++ src/views/Registration.tsx
@@ -186,133 +186,134 @@ const Registration = () => {
   };
 
   return (
-    <div style={{ backgroundImage: `url(${kuva})`, backgroundRepeat: 'no-repeat' , maxHeight: screenHeight,  minHeight: screenHeight , height: '100vh', backgroundSize: '100% 100%',  width: '100vw' }}>
+    <div style={{ backgroundImage: `url(${kuva})`, backgroundRepeat: 'no-repeat',  maxHeight: screenHeight,  minHeight: screenHeight , height: '99vh', backgroundSize: '100% 100%',  width: '99vw' }}>
         
-        { !showModal ? <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', width: '100%', height: '90%', alignSelf:'center', padding: 10}}>
-          <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', height: '100%', width: '100%', alignItems: 'center'}}>
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '25%'}}>
-              <label className='columnLabel'>Etunimi | Förnamn *</label>
-              <input 
-                data-testid='firstName'
-                className='inputStyle'
-                type="text" 
-                name="firstName" 
-                value={inputs?.firstName || ""} 
-                onChange={handleChange}
-              /> 
-            </div>
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '25%'}}>
-              <label className='columnLabel'>Sukunimi | Efternamn * </label>
-              <input
-                data-testid='lastName' 
-                className='inputStyle'
-                type="text" 
-                name="lastName" 
-                value={inputs?.lastName || ""} 
-                onChange={handleChange}
-              /> 
-            </div>
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '25%'}}>
-              <label className='columnLabel'> Sposti | Epost *</label>
-              <input
-                data-testid='email' 
-                className='inputStyle'
-                type="text" 
-                name="email" 
-                value={inputs?.email || ""} 
-                onChange={handleChange}
-              />
-            </div>
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '25%'}}>
-              <label className='columnLabel'>Ikä | Ålder *</label>
-              <input
-                data-testid='age' 
-                className='inputStyle'
-                type="text" 
-                name="age" 
-                value={inputs?.age || ""} 
-                onChange={handleChange}
-              /> 
-            </div>
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '25%'}}>
-              <label className='columnLabel'>Sukupuoli | Kön *</label>
-              <input
-                data-testid='gender' 
-                className='inputStyle'
-                type="text" 
-                name="gender" 
-                value={inputs?.gender || ""} 
-                onChange={handleChange}
-              />   
-            </div>
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '25%'}}>
-              <label className='columnLabel'> Seura | Förening </label>
-              <input
-                data-testid='team' 
-                className='inputStyle'
-                type="text" 
-                name="team" 
-                value={inputs?.team || ""} 
-                onChange={handleChange}
-              /> 
-            </div>
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '25%'}}>
-              <label className='columnLabel'> Puhelin | Telefon *</label>
-              <input
-                data-testid='phone' 
-                className='inputStyle'
-                type="text" 
-                name="phone" 
-                value={inputs?.phone || ""} 
-                onChange={handleChange}
-              />
-            </div>
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '25%'}}>
-              <label className='columnLabel'>Unisex koko/storlek *</label>
-                <div>
-                  <Select className='inputStyleTshirtTwo' value={{label: inputs.tshirt, value: inputs.tshirt}} options={options}  onChange={(e) => selectHandleChange(e)}   />
+        { !showModal ? 
+          <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100%', alignSelf:'center'}}>
+            <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', height: '100%', width: '100%', alignItems: 'center'}}>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '8%'}}>
+                <label className='columnLabel'>Etunimi | Förnamn *</label>
+                <input 
+                  data-testid='firstName'
+                  className='inputStyle'
+                  type="text" 
+                  name="firstName" 
+                  value={inputs?.firstName || ""} 
+                  onChange={handleChange}
+                /> 
+              </div>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '8%'}}>
+                <label className='columnLabel'>Sukunimi | Efternamn * </label>
+                <input
+                  data-testid='lastName' 
+                  className='inputStyle'
+                  type="text" 
+                  name="lastName" 
+                  value={inputs?.lastName || ""} 
+                  onChange={handleChange}
+                /> 
+              </div>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '8%'}}>
+                <label className='columnLabel'> Sposti | Epost *</label>
+                <input
+                  data-testid='email' 
+                  className='inputStyle'
+                  type="text" 
+                  name="email" 
+                  value={inputs?.email || ""} 
+                  onChange={handleChange}
+                />
+              </div>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '8%'}}>
+                <label className='columnLabel'>Ikä | Ålder *</label>
+                <input
+                  data-testid='age' 
+                  className='inputStyle'
+                  type="text" 
+                  name="age" 
+                  value={inputs?.age || ""} 
+                  onChange={handleChange}
+                /> 
+              </div>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '8%'}}>
+                <label className='columnLabel'>Sukupuoli | Kön *</label>
+                <input
+                  data-testid='gender' 
+                  className='inputStyle'
+                  type="text" 
+                  name="gender" 
+                  value={inputs?.gender || ""} 
+                  onChange={handleChange}
+                />   
+              </div>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '8%'}}>
+                <label className='columnLabel'> Seura | Förening </label>
+                <input
+                  data-testid='team' 
+                  className='inputStyle'
+                  type="text" 
+                  name="team" 
+                  value={inputs?.team || ""} 
+                  onChange={handleChange}
+                /> 
+              </div>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '8%'}}>
+                <label className='columnLabel'> Puhelin | Telefon *</label>
+                <input
+                  data-testid='phone' 
+                  className='inputStyle'
+                  type="text" 
+                  name="phone" 
+                  value={inputs?.phone || ""} 
+                  onChange={handleChange}
+                />
+              </div>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '8%'}}>
+                <label className='columnLabel'>Unisex koko/storlek *</label>
+                  <div>
+                    <Select className='inputStyleTshirtTwo' value={{label: inputs.tshirt, value: inputs.tshirt}} options={options}  onChange={(e) => selectHandleChange(e)}   />
+                  </div>
+              </div> 
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '8%'}}>
+                <label className='columnLabel'>Onko sinulla toimitsijakortti? | Har du domarkort? *</label>
+              
+                <Switch data-testid='licenseCard' onChange={handleLicenseCard} checked={inputs.licenseCard} />
+              </div>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '8%'}}>
+                <label className='columnLabel'>Tehtävätoivomus | Önskemål angående uppgift *</label>
+                <div className='taskList'>
+                  { box() }
                 </div>
-            </div> 
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '25%'}}>
-              <label className='columnLabel'>Onko sinulla toimitsijakortti? | Har du domarkort? *</label>
-            
-               <Switch data-testid='licenseCard' onChange={handleLicenseCard} checked={inputs.licenseCard} />
-            </div>
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '40%'}}>
-              <label className='columnLabel'>Tehtävätoivomus | Önskemål angående uppgift *</label>
-              <div className='taskList'>
-                { box() }
+              </div>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '5%'}}>
+                <label className='columnLabel'> Mitkä päivät olet kätettävissä | Vilka dagar kan Du ställa upp *</label>
+                <input  data-testid='first' type="checkbox" onClick={() => setInputs({...inputs, days: {...inputs.days, first: !inputs.days.first}})} /> <p className='dayLabel'>28.6.2024</p>
+                <input data-testid='second' type="checkbox" onClick={() => setInputs({...inputs, days: {...inputs.days, second: !inputs.days.second}})} /> <p className='dayLabel'>29.6.2024</p>
+                <input data-testid='third' type="checkbox" onClick={() => setInputs({...inputs, days: {...inputs.days, third: !inputs.days.third}})} /> <p className='dayLabel'>30.6.2024</p>
+              </div>
+              <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '5%'}}>
+                <label className='columnLabel'> Vapaat kommentit | Fria kommentarer *</label>
+                <textarea
+                  data-testid='freetextfield'
+                  name="freeText"
+                  className='inputStyleFreeText'
+                  rows={3}
+                  cols={40}
+                  onChange={(e) => handleChange(e)}
+                  value={inputs?.freeText}
+                />
+              </div>
+              <p style={{color:'green'}}> {responseMessage? responseMessage: null}</p>
+              {errorMessage? <p data-testid='errorMessage' style={{color:'red'}}> {errorMessage}</p> : null }
+              { validInputError ? <p style={{color:'red'}}> {validInputError}</p> : null }
+            </form>
+            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', alignSelf: 'center'}}>
+              <button type="submit"  onClick={() => handleSubmit()} data-testid='sendButtonTwo' disabled={!checked} > LÄHETÄ </button> 
+              <div style={{flexDirection:'row', display: 'flex'}}>
+                <input data-testid='robotButton' type="checkbox" onClick={() => setChecked(!checked)} /> <div style={{color: 'white'}}>En ole robotti</div>
               </div>
             </div>
-            <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '25%'}}>
-              <label className='columnLabel'> Mitkä päivät olet kätettävissä | Vilka dagar kan Du ställa upp *</label>
-              <input  data-testid='first' type="checkbox" onClick={() => setInputs({...inputs, days: {...inputs.days, first: !inputs.days.first}})} /> <p className='dayLabel'>28.6.2024</p>
-              <input data-testid='second' type="checkbox" onClick={() => setInputs({...inputs, days: {...inputs.days, second: !inputs.days.second}})} /> <p className='dayLabel'>29.6.2024</p>
-              <input data-testid='third' type="checkbox" onClick={() => setInputs({...inputs, days: {...inputs.days, third: !inputs.days.third}})} /> <p className='dayLabel'>30.6.2024</p>
-            </div>
-             <div style={{display: 'flex', flexDirection:  'row', width: '100%', height: '5%'}}>
-              <label className='columnLabel'> Vapaat kommentit | Fria kommentarer *</label>
-              <textarea
-                data-testid='freetextfield'
-                name="freeText"
-                className='inputStyleFreeText'
-                rows={3}
-                cols={40}
-                onChange={(e) => handleChange(e)}
-                value={inputs?.freeText}
-              />
-            </div>
-            <p style={{color:'green'}}> {responseMessage? responseMessage: null}</p>
-            {errorMessage? <p data-testid='errorMessage' style={{color:'red'}}> {errorMessage}</p> : null }
-            { validInputError ? <p style={{color:'red'}}> {validInputError}</p> : null }
-          </form>
-          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', alignSelf: 'center'}}>
-            <button type="submit"  onClick={() => handleSubmit()} data-testid='sendButtonTwo' disabled={!checked} > LÄHETÄ </button> 
-            <div style={{flexDirection:'row', display: 'flex'}}>
-              <input data-testid='robotButton' type="checkbox" onClick={() => setChecked(!checked)} /> <div style={{color: 'white'}}>En ole robotti</div>
-            </div>
-          </div>
-        </div> : ConfirmModal() }
+          </div> : ConfirmModal() }
       </div>
   )
 };
