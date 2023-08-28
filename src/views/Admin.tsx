c8136ef2e7442dcf85929d46abcea4a99938f883
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-08-28 11:48:46 +0300

    ip korjaus

--- src/views/Admin.tsx
+++ src/views/Admin.tsx
@@ -8,7 +8,7 @@ import * as ExcelJS from 'exceljs';
 import FileSaver from 'file-saver';
 
 const Admin = () => {
-    const ip =  '172.31.36.167';
+    const ip =  '13.51.169.250';
     const [inputs, setInputs] = useState< IAdmin >
     ({
       user: '',
